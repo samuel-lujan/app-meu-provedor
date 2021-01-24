6341863f332f4b842a1699ac70024481e40406f9
Samuel Lujan <samuel_lujan@hotmail.com>
2021-01-24 17:02:53 -0300

    Finalizando implementação do template

--- routes/web.php
+++ routes/web.php
@@ -13,9 +13,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Auth::routes();
+
+Route::get('/');
+Route::get('/home');
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
 Auth::routes();
 
